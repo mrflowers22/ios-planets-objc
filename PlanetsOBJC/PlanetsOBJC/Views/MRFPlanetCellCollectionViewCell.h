437f76afca30ead9fd88d9412eef448580fb4c09
Michael R.F <mrflowers22@uwalumni.com>
2019-10-31 09:47:57 -0700

    connected the iboutlets in the cell to its source code file

--- PlanetsOBJC/PlanetsOBJC/Views/MRFPlanetCellCollectionViewCell.h
+++ PlanetsOBJC/PlanetsOBJC/Views/MRFPlanetCellCollectionViewCell.h
@@ -12,6 +12,11 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface MRFPlanetCellCollectionViewCell : UICollectionViewCell
 
+@property (weak, nonatomic) IBOutlet UIImageView *planetImageView;
+@property (weak, nonatomic) IBOutlet UILabel *nameLabel;
+
+
+
 @end
 
 NS_ASSUME_NONNULL_END
